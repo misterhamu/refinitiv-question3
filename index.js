1cd7e0e3d33b464b196097250edbe5ed5f8f8412
Thitipat Na Nakorn <ihamjang@gmail.com>
2021-08-29 01:09:27 +0700

    done

---
+++ index.js
@@ -0,0 +1,17 @@
+const arg = process.argv[2];
+switch (arg) {
+    case "B-INCOMESSF":
+        console.log("10.0548");
+        break;
+    case "BM70SSF":
+        console.log("9.9774");
+        break;
+    case "BEQSSF":
+        console.log("11.247");
+        break;
+    case "B-FUTURESSF":
+        console.log("11.443");
+        break;
+    default:
+        break;
+}
